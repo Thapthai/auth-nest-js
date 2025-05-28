fb2ae0f8b83dd722bfbe16bf1282051deb0f6f25
Thapthai Rattananusarak <knight.thapthai@gmail.com>
2025-05-28 11:26:49 +0700

    Throttle Rate-Limiter api

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthenticationModule } from './authentication/authentication.module';
+import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
-  imports: [PrismaModule, AuthenticationModule],
+  imports: [
+    PrismaModule,
+    AuthenticationModule,
+    ThrottlerModule.forRoot([{
+      ttl: 60000,
+      limit: 10,
+    }]),
+  ],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule { }
--- src/authentication/authentication.controller.ts
+++ src/authentication/authentication.controller.ts
@@ -18,6 +18,7 @@ import { ResendVerificationDTO } from './dto/resendVerification.dto';
 import { ForgotPasswordDTO } from './dto/forgetPassword.dto';
 import { ResetPasswordDTO } from './dto/resetPassword.dto';
 import { twoFAEnableDTO } from './dto/twoFAEnable.dto';
+import { Throttle } from '@nestjs/throttler';
 
 @Controller('auth')
 export class AuthController {
@@ -26,12 +27,15 @@ export class AuthController {
     private readonly sendVerifyEmail: VerifyEmail,
   ) { }
 
+  @Throttle({ default: { limit: 10, ttl: 60000 } })
   @Post('register')
   async register(@Body() body: RegisterDTO) {
     const user = await this.authService.register(body);
     return { message: 'User registered', userId: user.id };
   }
 
+
+  @Throttle({ default: { limit: 10, ttl: 60000 } })
   @Post('login')
   @HttpCode(201)
   async login(@Body() body: LoingDTO) {
@@ -59,6 +63,7 @@ export class AuthController {
     return this.authService.resendVerificationEmail(dto);
   }
 
+  @Throttle({ default: { limit: 10, ttl: 60000 } })
   @Post('2fa/verify')
   async verify2FA(@Body() body) {
     const isValid = await this.authService.verify2FA(body.userId, body.code);
@@ -66,6 +71,7 @@ export class AuthController {
     const user = await this.authService.getUserById(body.userId);
   }
 
+  @Throttle({ default: { limit: 10, ttl: 60000 } })
   @UseGuards(JwtAuthGuard)
   @Post('2fa/generate2FASecret')
   async setup2FA(@Req() req) {
@@ -85,6 +91,7 @@ export class AuthController {
     return this.authService.enable2FA(dto);
   }
 
+  @Throttle({ default: { limit: 10, ttl: 60000 } })
   @UseGuards(JwtAuthGuard)
   @Post('2fa/login')
   async loginWith2FA(@Body() body) {

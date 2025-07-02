4360809e1cd29f08ca961b660706edf8dd871442
Thapthai Rattananusarak <knight.thapthai@gmail.com>
2025-07-02 09:47:33 +0700

    Update CORS configuration in main.ts to allow requests from an additional origin

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,10 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: process.env.CORS_ORIGIN?.split(',') || ['http://localhost:3005'],
+    origin: [
+      'http://localhost:3005',
+      'http://10.11.9.43:3005'
+    ],
     credentials: true,
   })
   app.useGlobalPipes(new ValidationPipe());
